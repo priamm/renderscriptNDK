679fcfc0f3f9acdffe39a458b1aff3114c1d3f9c
priam <priammmm@yandex.ru>
2019-09-03 15:43:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/renderscript/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.renderscript
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        sample_text.text = stringFromJNI(cacheDir.toString())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private external fun stringFromJNI(appCacheDir: String): String
+
+    companion object {
+
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
